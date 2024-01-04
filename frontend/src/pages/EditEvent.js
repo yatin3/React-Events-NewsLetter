ff01b684ca3e4c381c1f8000fb679cbfbb1a8774
yatin goyal <yatingoyal31@gmail.com>
2024-01-04 14:15:36 +0530

    added event edit form

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,5 +1,12 @@
+import { useRouteLoaderData } from 'react-router-dom';
+
+import EventForm from "../components/EventForm";
+
 function EditEventPage(){
-    return <h1>EditEventPage</h1>;
+  const data = useRouteLoaderData('event-detail');
+  const event = data.event;
+
+    return <EventForm event={event}/>;
   }
   
   export default EditEventPage;
