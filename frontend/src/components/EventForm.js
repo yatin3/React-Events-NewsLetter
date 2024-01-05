f0b5a1521719a860ea3f35e63af9d8362f05af24
yatin goyal <yatingoyal31@gmail.com>
2024-01-05 12:06:07 +0530

    submitting and editing

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,45 +1,121 @@
-import { Form,useNavigation } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import {
+  Form,
+  useNavigation,
+  useActionData,
+  json,
+  redirect,
+} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import classes from './EventForm.module.css';
+import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
-  const isSubmittting = navigation.state == 'submitting';
-
+  const isSubmittting = navigation.state == "submitting";
 
   function cancelHandler() {
-    navigate('..');
+    navigate("..");
   }
 
   return (
-    <Form method='post' className={classes.form}>
+    <Form method={method} className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required defaultValue={event ? event.title : ''}/>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          required
+          defaultValue={event ? event.title : ""}
+        />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required defaultValue={event ? event.image : ''}/>
+        <input
+          id="image"
+          type="url"
+          name="image"
+          required
+          defaultValue={event ? event.image : ""}
+        />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event ? event.date : ''}/>
+        <input
+          id="date"
+          type="date"
+          name="date"
+          required
+          defaultValue={event ? event.date : ""}
+        />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''}/>
+        <textarea
+          id="description"
+          name="description"
+          rows="5"
+          required
+          defaultValue={event ? event.description : ""}
+        />
       </p>
       <div className={classes.actions}>
         <button type="button" onClick={cancelHandler}>
           Cancel
         </button>
-        <button disabled={isSubmittting}>{isSubmittting ? 'submitting...' : 'save'}</button>
+        <button disabled={isSubmittting}>
+          {isSubmittting ? "submitting..." : "save"}
+        </button>
       </div>
     </Form>
   );
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  let url = "http://localhost:8080/events";
+
+  if (method === "PATCH") {
+    const eventId = params.eventId;
+    url = "http://localhost:8080/events/" + eventId;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "could not save event!" }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -6,7 +6,7 @@ function EditEventPage(){
   const data = useRouteLoaderData('event-detail');
   const event = data.event;
 
-    return <EventForm event={event}/>;
+    return <EventForm method="PATCH" event={event}/>;
   }
   
   export default EditEventPage;
